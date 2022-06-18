5c02a0a5ae274e960a73c7febe9d5c7f427c1d66
mrmohsensami <mrmohsensami@gmail.com>
2022-06-18 11:11:08 +0430

    Blog app - model added - admin added - return HTTPResponse

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Blog(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'draft'),
+        ('p', 'publish')
+    )
+    title = models.CharField(max_length=200, verbose_name='Title')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='Slug')
+    body = models.TextField()
+    image = models.ImageField(upload_to='uploads/', verbose_name='Image')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='Active/DeActive')
+    publish = models.DateTimeField(default=timezone.now, verbose_name='Publish')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        return self.title
+
+
