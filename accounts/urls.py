98e700cc8833a6ae73a4b726e1833b31db84e084
mrmohsensami <mrmohsensami@gmail.com>
2022-06-19 15:54:49 +0430

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'account'
 urlpatterns = [
-    path('register/', views.user_register),
-    path('login/', views.user_login),
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
